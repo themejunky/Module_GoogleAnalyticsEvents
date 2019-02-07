e6123b18d00af987b11809927b5c0b6325feda41
alinlastun <alinlastun@gmail.com>
2019-02-07 17:08:16 +0200

    mic 4

--- app/src/main/java/com/themejunky/module/google/analytics/event/MainActivity.java
+++ app/src/main/java/com/themejunky/module/google/analytics/event/MainActivity.java
@@ -3,11 +3,14 @@ package com.themejunky.module.google.analytics.event;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import module.themejunky.com.tj_gae.Module_GoogleAnalyticsEvents;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Module_GoogleAnalyticsEvents.getInstance(this,"asdfasd");
     }
 }
